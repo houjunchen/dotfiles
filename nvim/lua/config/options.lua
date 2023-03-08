f111a5399c29a6be65ebb526e72b9ceacf067aaf
Abner Chen <houjunchen@gmail.com>
2023-03-08 14:35:50 +0800

    Add config for neovide

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -10,3 +10,9 @@ vim.opt.smartindent = true
 vim.opt.smarttab = true
 -- vim.opt.tabstop = 4
 vim.opt.wrap = true
+
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  vim.opt.guifont = "SauceCodePro Nerd Font Mono:h13"
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+end
