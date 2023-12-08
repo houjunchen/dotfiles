70ff6329f3be906815aa0f2bcef3d9999b78f3c9
Abner Chen <houjunchen@gmail.com>
2023-12-08 10:43:41 +0800

    Update lazyvim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -7,7 +7,7 @@ vim.api.nvim_create_autocmd("FileType", {
   command = "setlocal shiftwidth=4 tabstop=4",
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "javascript",
-  command = "setlocal shiftwidth=4 tabstop=4",
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "javascript",
+--   command = "setlocal shiftwidth=4 tabstop=4",
+-- })
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,5 @@
 return {
   {
-    "RRethy/nvim-base16",
-    config = function()
-      require("base16-colorscheme").with_config({
-        telescope = false,
-      })
-    end,
-  },
-
-  { "ellisonleao/gruvbox.nvim" },
-
-  {
     "catppuccin/nvim",
     name = "catppuccin",
     opts = {
