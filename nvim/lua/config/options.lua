64cb6eaea5473aa46c36173101433c4157719188
Abner Chen <houjunchen@gmail.com>
2024-11-18 21:54:22 +0800

    Update lazyvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.spell = false
 vim.opt.expandtab = true
 vim.opt.foldlevel = 99
 vim.opt.foldmethod = "indent"
