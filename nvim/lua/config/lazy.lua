ee962c434650d3f8d58ab933060bf3d214e03529
Abner Chen <houjunchen@gmail.com>
2024-12-17 18:06:07 +0800

    Update lazyvim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -11,18 +11,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
-    {
-      import = "plugins",
-      cond = function()
-        return not vim.g.vscode
-      end,
-    },
-    {
-      import = "lazyvim.plugins.extras.vscode",
-      cond = function()
-        return vim.g.vscode
-      end,
-    },
+    { import = "plugins" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,6 +1,7 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    scroll = { enabled = false },
     dashboard = {
       preset = {
         header = [[
